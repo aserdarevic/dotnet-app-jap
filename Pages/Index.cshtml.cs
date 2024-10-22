883e9894716721c1b20b897bddcf12be604aa3c3
unknown <aserdarevi1@etf.unsa.ba>
2024-10-22 08:54:29 +0200

    Enable env variables

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,22 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using System.Runtime.InteropServices;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Options;
+using dotnetcoresample.Models;
 
-namespace dotnetcoresample.Pages;
-
-public class IndexModel : PageModel
+namespace dotnetcoresample.Pages
 {
-
-    public string OSVersion { get { return RuntimeInformation.OSDescription; }  }
-    
-    private readonly ILogger<IndexModel> _logger;
-
-    public IndexModel(ILogger<IndexModel> logger)
+    public class IndexModel : PageModel
     {
-        _logger = logger;
-    }
+        private readonly CustomSettings _customSettings;
 
-    public void OnGet()
-    {        
+        public IndexModel(IOptions<CustomSettings> customSettings)
+        {
+            _customSettings = customSettings.Value;
+        }
+
+        public string Var1 { get; private set; }
+        public string Var2 { get; private set; }
+
+        public void OnGet()
+        {
+            Var1 = _customSettings.Var1;
+            Var2 = _customSettings.Var2;
+        }
     }
 }
